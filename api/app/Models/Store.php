826b6130c6f4450b15031b4e062cd58197d301a7
Ahykaa <patino,airica@gmail.com>
2023-12-14 02:30:22 +0800

    Fix code comment and Add relations to model

---
+++ api/app/Models/Store.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Store extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'label',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+}
