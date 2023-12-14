22c75b91ebffbc3ce25cf98ad5541e5dd35619fd
Aic Patiño <89176216+Ahykaa@users.noreply.github.com>
2023-12-14 15:29:21 +0800

    Fix code comment and Add relations to model (#5)
    * Fix code comment and Add relations to model
    
    * Fix code comment
    
    ---------
    
    Co-authored-by: Ahykaa <patino,airica@gmail.com>

---
+++ api/database/migrations/2023_12_13_180132_create_orders_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->foreignId('store_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->foreignId('category_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->integer('job_order')->nullable();
+            $table->integer('order_number');
+            $table->string('brand');
+            $table->string('model')->nullable();
+            $table->decimal('downpayment')->default(0);
+            $table->integer('quantity');
+            $table->enum('status', ['delivered', 'open', 'in-transit', 'cancelled', 'onhold'])->default('open');
+            $table->string('link')->nullable();
+            $table->text('notes')->nullable();
+            $table->jsonb('image_paths')->nullable();
+            $table->dateTime('order_at')->default(now());
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
