93f6c9f34aedd37156dc2dabafde2df289d76b80
Aic Patiño <89176216+Ahykaa@users.noreply.github.com>
2023-12-15 09:31:18 +0800

    Aica/db seeders (#8)
    * Add seeders
    
    * Fix code comment
    
    ---------
    
    Co-authored-by: Ahykaa <patino,airica@gmail.com>

---
+++ api/database/seeders/CategorySeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB; 
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('categories')->upsert([
+            ['label' => 'Charger'],
+            ['label' => 'Battery'],
+            ['label' => 'Keyboard'],
+            ['label' => 'Motherboard'],
+            ['label' => 'Flex'],
+            ['label' => 'LCD'],
+            ['label' => 'Case'],
+            ['label' => 'Speaker'],
+            ['label' => 'Fan'],
+        ],  ['label']);
+    }
+}
