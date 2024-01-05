59019258c2d047f817794c09f75c2fa5b76f7d7e
Kiel <89174822+Kiel-1401@users.noreply.github.com>
2024-01-05 21:44:59 +0800

    Tonette/UI add staff page (#16)
    * ui-add-staff-page
    
    * ui-create-staff-page
    
    * Update Comment
    
    * Corrected Version of Add Staff page
    
    * Removed the height property of submit button

---
+++ web/src/pages/staffs/new/index.page.jsx
@@ -0,0 +1,28 @@
+import { Button } from 'flowbite-react'
+import React from 'react'
+
+import TextInput from '@/components/organisms/TextInput'
+import Template from '@/components/templates/Template'
+
+const AddStaff = () => {
+  return (
+    <Template>
+      <form className='flex w-80 flex-col gap-2'>
+        <TextInput label='Name' name='createStaff' />
+        <TextInput label='Position' name='createStaff' />
+        <TextInput label='Contact Number' name='createStaff' />
+        <TextInput label='Account Type' name='createStaff' />
+
+        <Button
+          color='warning'
+          type='submit'
+          style={{ width: 140}}
+        >
+          Submit
+        </Button>
+      </form>
+    </Template>
+  )
+}
+
+export default AddStaff
