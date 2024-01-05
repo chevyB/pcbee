b372753a96375a2a6d89351a342a4b63c894fd76
Ahykaa <patino,airica@gmail.com>
2024-01-05 18:25:42 +0800

    2 columns

--- web/src/pages/orders/new/index.page.jsx
+++ web/src/pages/orders/new/index.page.jsx
@@ -1,44 +1,39 @@
-import { Button } from "flowbite-react";
-import React from 'react';
+import { Button } from 'flowbite-react'
+import React from 'react'
 
-import DatePicker from '@/components/organisms/DatePicker';
-import FilePickerInput from '@/components/organisms/FilePickerInput ';
-import SelectInput from '@/components/organisms/SelectInput';
-import TextAreaInput from '@/components/organisms/TextAreaInput';
-import TextInput from "@/components/organisms/TextInput";
+import DatePicker from '@/components/organisms/DatePicker'
+import FilePickerInput from '@/components/organisms/FilePickerInput '
+import SelectInput from '@/components/organisms/SelectInput'
+import TextAreaInput from '@/components/organisms/TextAreaInput'
+import TextInput from '@/components/organisms/TextInput'
 import Template from '@/components/templates/Template'
 
 const Order = () => {
-
-    const staffOptions = [
+    const branchOptions = [
         {
             value: 0,
-            label: 'Select Staff Name',
+            label: 'Branch',
             disabled: true,
             selected: true,
             hidden: true,
         },
         {
             value: 1,
-            label: 'Leizl'
+            label: 'Cebu',
         },
         {
             value: 2,
-            label: 'Kendra'
+            label: 'Davao',
         },
         {
             value: 3,
-            label: 'Narcisa'
+            label: 'Tagum',
         },
-        {
-            value: 4,
-            label: 'Leila'
-        },
-    ];
+    ]
     const statusOptions = [
         {
             value: 0,
-            label: 'Select Status',
+            label: 'Status',
             disabled: true,
             selected: true,
             hidden: true,
@@ -63,30 +58,52 @@ const Order = () => {
             value: 5,
             label: 'Onhold',
         },
-    ];
-
+    ]
     return (
         <Template>
-        <section className="bg-white dark:bg-gray-900">
-            <div className="container mx-auto px-8 py-8">
-                <form className="mx-auto">
-                    <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 w-full">
+        <section className='bg-white dark:bg-gray-900'>
+            <div className='container mx-auto px-8 py-8'>
+                <form className='grid lg:grid-cols-2 gap-4'>
+                    <div className='grid grid-cols-1 gap-4'>
+                        {/* Inputs in the first column */}
                         <DatePicker label='Date' name='created_at' />
                         <TextInput label='Job Order No.' name='job_order' />
+                        <SelectInput
+                            label='Status'
+                            name='status'
+                            options={statusOptions}
+                        />
                         <TextInput label='Category' name='category_id' />
                         <TextInput label='Brand' name='brand' />
-                        <TextInput label='Unit Model' name='model' />
                         <TextInput label='Parts Model' name='part_model' />
                         <TextInput label='Quantity' name='quantity' />
                         <TextInput label='Link Ref' name='link' />
-                        <TextInput label='Downpayment' name='downpayment' />
-                        <TextInput label='Branch Name' name='Store__id' />
-                        <SelectInput label='Staff Name' name='user_id' options={staffOptions} />
-                        <SelectInput label='Status' name='status' options={statusOptions} />
-                        <TextAreaInput label='Comments' name='notes' placeHolder='Comments' />
-                        <FilePickerInput label='Upload File' name='images_paths' multiple />
+                        <TextInput label='Down Payment' name='downpayment' />
+                        <FilePickerInput
+                            label='Upload File'
+                            name='images_paths'
+                            multiple
+                        />
                     </div>
-                    <div className="mt-8 ">
+                    <div className='grid grid-cols-1 gap-4'>
+                        {/* Inputs in the second column */}
+                        <SelectInput
+                            label='Branch'
+                            name='store__id'
+                            options={branchOptions}
+                        />
+                        <TextInput label='Unit Model' name='unit_model' />
+                    </div>
+                    <div className='col-span-full'>
+                        {/* Textarea for comments */}
+                        <TextAreaInput
+                            label='Comments'
+                            name='notes'
+                            placeHolder='Comments'
+                        />
+                    </div>
+                    <div className='col-span-full'>
+                        {/* Submit button */}
                         <Button color='warning' type='submit'>
                             Submit
                         </Button>
@@ -94,8 +111,8 @@ const Order = () => {
                 </form>
             </div>
         </section>
-        </Template>
-    );
-};
+    </Template>
+)
+}
 
-export default Order;
+export default Order
