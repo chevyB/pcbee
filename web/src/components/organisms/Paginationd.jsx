eccb8d05fcb16be1ab3c865fddbfb3902b9152a3
ccmanlapus <manlapus98@gmail.com>
2024-01-27 12:37:45 +0800

    deleted npm

---
+++ web/src/components/organisms/Paginationd.jsx
@@ -0,0 +1,17 @@
+// Pagination.jsx
+import { Pagination as FlowbitePagination } from 'flowbite-react';
+import { useState } from 'react';
+
+const Pagination = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const onPageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div className="flex overflow-x-auto sm:justify-center">
+      <FlowbitePagination currentPage={currentPage} totalPages={100} onPageChange={onPageChange} />
+    </div>
+  );
+}
+
+export default Pagination;
