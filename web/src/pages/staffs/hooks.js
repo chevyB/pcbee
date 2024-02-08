219fe4b0ecce21549b644c58f73f634987edd93c
Aic Patiño <89176216+Ahykaa@users.noreply.github.com>
2024-02-08 23:01:48 +0800

    Staff Pagination Integration Update (#36)
    Co-authored-by: Ahykaa <patino,airica@gmail.com>

--- web/src/pages/staffs/hooks.js
+++ web/src/pages/staffs/hooks.js
@@ -1,17 +1,14 @@
 import { useRouter } from 'next/router'
-import { useState } from 'react'
 import { FaUserFriends } from 'react-icons/fa'
 
 import { useUsers } from '@/hooks/redux/useUsers'
 
 const useHooks = () => {
   const router = useRouter()
-  const [currentPage, setCurrentPage] = useState(router.query.page || 1)
 
-  const { users, totalPages, isLoading } = useUsers(currentPage)
+  const { users, totalPages, isLoading } = useUsers(router.query.page || 1)
 
   const onPageChange = (page) => {
-    setCurrentPage(page)
     router.push({ pathname: '/staffs', query: { page } })
   }
 
@@ -27,7 +24,7 @@ const useHooks = () => {
     users,
     isLoading,
     totalPages,
-    currentPage,
+    currentPage: router.query.page || 1,
     onPageChange,
     breadcrumbs,
   }
