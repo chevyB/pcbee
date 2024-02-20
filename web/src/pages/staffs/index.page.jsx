db7a263b13246d368e496460424c756b132f5471
chevyB <chevbanico@gmail.com>
2024-02-20 15:21:06 +0800

    page restriction

--- web/src/pages/staffs/index.page.jsx
+++ web/src/pages/staffs/index.page.jsx
@@ -13,6 +13,7 @@ import React from 'react'
 import Loading from '@/components/atoms/Loading'
 import PageHeader from '@/components/organisms/PageHeader'
 import Pagination from '@/components/organisms/Pagination'
+import AdminGuard from '@/components/templates/AdminGuard'
 import Template from '@/components/templates/Template'
 
 import useHooks from './hooks'
@@ -29,46 +30,48 @@ const Dashboard = () => {
 
   return (
     <Template>
-      <PageHeader
-        breadcrumbs={breadcrumbs}
-        right={
-          <Link href='/staffs/new'>
-            <Button size='xs' color='warning'>
-              Create Staff
-            </Button>
-          </Link>
+      <AdminGuard>
+        <PageHeader
+          breadcrumbs={breadcrumbs}
+          right={
+            <Link href='/staffs/new'>
+              <Button size='xs' color='warning'>
+                Create Staff
+              </Button>
+            </Link>
+          }
+        />
+        {isLoading ?
+          <Loading />
+        : <Table>
+            <TableHead>
+              <TableHeadCell>Name</TableHeadCell>
+              <TableHeadCell>Username</TableHeadCell>
+              <TableHeadCell>Phone</TableHeadCell>
+              <TableHeadCell>Position</TableHeadCell>
+              <TableHeadCell>Role</TableHeadCell>
+            </TableHead>
+            <TableBody>
+              {users &&
+                users.map((user) => (
+                  <TableRow key={user.id}>
+                    <TableCell>{user.name}</TableCell>
+                    <TableCell>{user.username}</TableCell>
+                    <TableCell>{user.phone}</TableCell>
+                    <TableCell>{user.position}</TableCell>
+                    <TableCell>{user.role}</TableCell>
+                  </TableRow>
+                ))}
+            </TableBody>
+          </Table>
         }
-      />
-      {isLoading ?
-        <Loading />
-      : <Table>
-          <TableHead>
-            <TableHeadCell>Name</TableHeadCell>
-            <TableHeadCell>Username</TableHeadCell>
-            <TableHeadCell>Phone</TableHeadCell>
-            <TableHeadCell>Position</TableHeadCell>
-            <TableHeadCell>Role</TableHeadCell>
-          </TableHead>
-          <TableBody>
-            {users &&
-              users.map((user) => (
-                <TableRow key={user.id}>
-                  <TableCell>{user.name}</TableCell>
-                  <TableCell>{user.username}</TableCell>
-                  <TableCell>{user.phone}</TableCell>
-                  <TableCell>{user.position}</TableCell>
-                  <TableCell>{user.role}</TableCell>
-                </TableRow>
-              ))}
-          </TableBody>
-        </Table>
-      }
 
-      <Pagination
-        currentPage={currentPage}
-        onPageChange={onPageChange}
-        totalPages={totalPages}
-      />
+        <Pagination
+          currentPage={currentPage}
+          onPageChange={onPageChange}
+          totalPages={totalPages}
+        />
+      </AdminGuard>
     </Template>
   )
 }
